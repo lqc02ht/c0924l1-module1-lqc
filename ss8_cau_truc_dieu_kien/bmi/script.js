a10db608cfa75cbcfc7c24c1aad30a819cd23ad8
sffs761 <cuong02ht@gmail.com>
2024-10-18 17:39:42 +0700

    fixed

--- ss8_cau_truc_dieu_kien/bmi/script.js
+++ ss8_cau_truc_dieu_kien/bmi/script.js
@@ -1,14 +1,14 @@
 function countBMI() {
     let weight = parseFloat(document.getElementById("weight").value);
     let height = parseFloat(document.getElementById("height").value);
-    let bmi = weight / (height*height)
+    let bmi = weight / (height*height);
     if (bmi >= 30) {
-        document.getElementById("result").innerText = "Your result: Obese"
+        document.getElementById("result").innerText = "Your result: Obese";
     } else if (bmi >= 25 && bmi < 30) {
-        document.getElementById("result").innerText = "Your result: Overweight"
+        document.getElementById("result").innerText = "Your result: Overweight";
     } else if (bmi >= 18.5 && bmi < 25) {
-        document.getElementById("result").innerText = "Your result: Normal"
+        document.getElementById("result").innerText = "Your result: Normal";
     } else {
-        document.getElementById("result").innerText = "Your result: Underweight"
+        document.getElementById("result").innerText = "Your result: Underweight";
     }
 }
