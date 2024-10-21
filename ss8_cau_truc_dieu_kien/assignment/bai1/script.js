aa7223a483e333709547ddd4376994e0809afc10
sffs761 <cuong02ht@gmail.com>
2024-10-22 00:19:15 +0700

    rename

--- ss8_cau_truc_dieu_kien/assignment/bai1/script.js
+++
@@ -1,9 +0,0 @@
-function check() {
-    let a = parseFloat(document.getElementById("number1").value);
-    let b = parseFloat(document.getElementById("number2").value);
-    if (a % b === 0) {
-        document.getElementById("result").innerText = "a chia hết cho b";
-    } else {
-        document.getElementById("result").innerText = "a không chia hết cho b";
-    }
-}
--- ss8_cau_truc_dieu_kien/assignment/bai2/script.js
+++
@@ -1,8 +0,0 @@
-function checkAge() {
-    let a = parseFloat(document.getElementById("age").value);
-    if (a >= 15) {
-        document.getElementById("result").innerText = "Học sinh đủ điều kiện vào học lớp 10!";
-    } else {
-        document.getElementById("result").innerText = "Học sinh không đủ điều kiện vào học lớp 10!";
-    }
-}
